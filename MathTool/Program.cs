bb96c7320e8f241dd8d90b0dcb0151bfd2e7206b
Kian738 <56565697+Kian738@users.noreply.github.com>
2024-06-10 16:53:53 +0200

    STYLE: Add missing space in -x

--- MathTool/Program.cs
+++ MathTool/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main()
     {
         // TODO: Use a testing framework
-        List<string> testFunctionStrings = ["-x^2 + 5x - 1", "3x^2 - (1+3)x" /* TODO: Fix */, "3x^2 - (1/3)x", "24,62x + 5", "2x^4 - 6x^3 + x^2 + x^0,5 - 4", "-4x^2 - +2x + -2" /* TODO: Fix */, "-x^2 -x + -1"]; // TODO: Support f(x) =
+        List<string> testFunctionStrings = ["-x^2 + 5x - 1", "3x^2 - (1+3)x" /* TODO: Fix */, "3x^2 - (1/3)x", "24,62x + 5", "2x^4 - 6x^3 + x^2 + x^0,5 - 4", "-4x^2 - +2x + -2" /* TODO: Fix */, "-x^2 - x + -1"]; // TODO: Support f(x) =
         foreach (var functionString in testFunctionStrings)
         {
             var function = new Function(functionString);
